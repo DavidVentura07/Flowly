735a7d65e6e43b433607a575afe294b184a1f0c6
DavidVentura07 <david.ventura.arqunam@gmail.com>
2026-06-02 20:15:20 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'flowly2-v5';
+const CACHE = 'flowly2-v6';
 const ASSETS = ['./', './index.html', './style.css', './app.js', './data.js', './manifest.json',
   'https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,700;1,400&family=DM+Sans:wght@300;400;500&display=swap'];
 

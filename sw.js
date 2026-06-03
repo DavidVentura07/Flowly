4a796698ccfe4ffdf10846266ed1eb1f8adba084
DavidVentura07 <david.ventura.arqunam@gmail.com>
2026-06-02 20:06:56 -0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE = 'flowly2-v5';
+const ASSETS = ['./', './index.html', './style.css', './app.js', './data.js', './manifest.json',
+  'https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,700;1,400&family=DM+Sans:wght@300;400;500&display=swap'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+self.addEventListener('fetch', e => {
+  // Network first for HTML/CSS/JS so updates are always fresh
+  const url = new URL(e.request.url);
+  const isLocal = url.origin === self.location.origin;
+  if (isLocal) {
+    e.respondWith(
+      fetch(e.request)
+        .then(res => {
+          const clone = res.clone();
+          caches.open(CACHE).then(c => c.put(e.request, clone));
+          return res;
+        })
+        .catch(() => caches.match(e.request))
+    );
+  } else {
+    e.respondWith(caches.match(e.request).then(cached => cached || fetch(e.request)));
+  }
+});
